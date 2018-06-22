ea99beb3934676da13db99bcc02ef996b5f94d54
Luis Farfan <lucho.farfan9@gmail.com>
2018-06-22 11:19:45 -0500

    note component created2

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotesComponent } from './notes/notes.component';
+import {MatCardModule} from '@angular/material';
+import {LoginComponent} from '../login/login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: '', component: NotesComponent },
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    RouterModule.forChild(appRoutes)
   ],
-  declarations: []
+  declarations: [NotesComponent]
 })
 export class NotesModule { }
